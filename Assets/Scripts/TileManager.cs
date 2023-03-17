cd47757b1100d5d705000683bfd6c1508a9789fd
ZionFrost <inaki337@gmail.com>
2023-03-17 15:28:23 +0100

    ADDED Basic Level generation

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] StreetTiles;
+    public GameObject[] SidewalkRight;
+    public GameObject[] SidewalkLeft;
+    public List<GameObject> ActiveTiles = new();
+
+    public Transform Player;
+
+    public float ZSpawn = -9;
+    public float TileLenght = 3;
+
+    public int NumberOfTIles =20    ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i  = 0; i < NumberOfTIles; i++)
+        SpawnTile(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player.position.z - TileLenght *3 > ZSpawn - (NumberOfTIles * TileLenght))
+        {
+            SpawnTile(0,0,0);
+            DeleteTile();
+        }
+    }
+
+    public void SpawnTile(int tileIndexStreet, int tileIndexSidewalkLeft, int tileIndexSidewalkRight)
+    {
+        //Prototype code
+        ActiveTiles.Add(Instantiate(StreetTiles[tileIndexStreet], transform.forward * ZSpawn, transform.rotation, transform));
+        ActiveTiles.Add(Instantiate(SidewalkLeft[tileIndexSidewalkLeft], transform.forward * ZSpawn + transform.right *3, transform.rotation,transform));
+        ActiveTiles.Add(Instantiate(SidewalkRight[tileIndexSidewalkRight], transform.forward * ZSpawn - transform.right * 3, transform.rotation, transform));
+
+        ZSpawn += TileLenght;
+    }
+
+    private void DeleteTile()
+    {
+        for(int i = 0; i <3; i++)
+        {
+            Destroy(ActiveTiles[0]);
+            ActiveTiles.RemoveAt(0);
+        }
+    }
+
+
+
+}
