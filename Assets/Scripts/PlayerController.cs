d73bd075a3e00d277c3d7d06fef06ed9a5aa6e6e
ZionFrost <inaki337@gmail.com>
2023-03-18 16:34:22 +0100

    Added the slide

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,11 +15,18 @@ public class PlayerController : MonoBehaviour
     private bool _isGrounded;
 
 
+    [Header("Slide")]
+    public float SlideDuration;
+    private float _slideTime;
+    public Transform _slidePivot;
+    public bool _isSliding;
+ 
+
+
     private Rigidbody _rigidbody;
     
 
     public Vector3 _movement;
-    private Vector3 _startMovePos;
     private float xPos;
     private bool _isSwapingLanes;
 
@@ -33,23 +40,39 @@ public class PlayerController : MonoBehaviour
     {
         _isGrounded = Physics.Raycast(transform.position, Vector3.down, PlayerHeight * 0.5f + 0.2f, GroundMask);
 
-        
-
         if (_isSwapingLanes & transform.position.x == xPos) _isSwapingLanes = false;
     }
 
     private void FixedUpdate()
     {
-      
-
         var pos = transform.position;
         _rigidbody.MovePosition(pos + new Vector3(0, 0, WalkSpeed * Time.deltaTime));
+
         if (_isSwapingLanes)
         _rigidbody.MovePosition(Vector3.MoveTowards(pos, new Vector3(xPos, pos.y, pos.z),Time.deltaTime*LaneSwapSpeed));
 
 
-       
-            //(new Vector3(0, 0, WalkSpeed), ForceMode.Force);
+
+        if (_isSliding)
+        {
+            _slideTime += Time.deltaTime;
+
+            if (_slideTime > SlideDuration)
+            {
+                _isSliding = false;
+                _slideTime = 0;
+            }
+               
+
+            _slidePivot.rotation = Quaternion.Euler(new Vector3(-90, 0, 0));
+
+        }
+        else if (!_isSliding )
+        {
+            _slidePivot.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+
+        }
+
         
     }
 
@@ -57,13 +80,24 @@ public class PlayerController : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed & _isGrounded)  
+        if (context.performed && _isGrounded)  
         _rigidbody.AddForce(transform.up * JumpForce, ForceMode.Impulse);
+    }
+
+    public void Slide(InputAction.CallbackContext context)
+    {
+       
+        if (context.performed && _isGrounded && !_isSliding)
+        {
+             Debug.Log("Slide");
+            _isSliding = true;
+        }
+           
     }
 
     public void HorizontalMovement(InputAction.CallbackContext context)
     {
-        if (context.performed & !_isSwapingLanes)
+        if (context.performed && !_isSwapingLanes)
         {
             _isSwapingLanes = true;
             float value = context.ReadValue<float>();
